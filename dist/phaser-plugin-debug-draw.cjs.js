e67df4709c078633d230f671a98d4a29a5055ccb
samme <samme@users.noreply.github.com>
2019-11-26 12:58:49 -0800

    4.0.0

--- dist/phaser-plugin-debug-draw.cjs.js
+++ dist/phaser-plugin-debug-draw.cjs.js
@@ -5,18 +5,10 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 var Phaser = _interopDefault(require('phaser'));
 
 var cos = Math.cos;
+var max = Math.max;
 var sin = Math.sin;
-var _inputs = [];
-var _masks = [];
+
 var POINTER_RADIUS = 20;
-
-function getLeft (obj) {
-  return obj.originX ? (obj.x - obj.originX * obj.displayWidth) : obj.x;
-}
-
-function getTop (obj) {
-  return obj.originY ? (obj.y - obj.originY * obj.displayHeight) : obj.y;
-}
 
 var DebugDrawPlugin = /*@__PURE__*/(function (superclass) {
   function DebugDrawPlugin () {
@@ -50,33 +42,46 @@ var DebugDrawPlugin = /*@__PURE__*/(function (superclass) {
   };
 
   DebugDrawPlugin.prototype.drawAll = function drawAll () {
-    var inputs = _inputs;
-    var masks = _masks;
     var ref = this.systems;
     var displayList = ref.displayList;
-    var input = ref.input;
 
     if (!displayList.length) { return; }
 
-    inputs.length = 0;
-    masks.length = 0;
+    var disabledInputs = [];
+    var inputs = [];
+    var masks = [];
+    var showInput = this.showInput && this.systems.input.isActive();
 
     this.graphic.clear()
       .fillStyle(this.color, this.alpha)
       .lineStyle(this.lineWidth, this.color, this.alpha);
 
-    displayList.each(this.processObj, this, inputs, masks);
+    displayList.each(this.processObj, this, disabledInputs, inputs, masks, showInput);
 
-    if (inputs.length) { this.drawInputs(inputs); }
-    if (masks.length) { this.drawMasks(masks); }
-    if (input.enabled && this.showPointers) { this.drawPointers(this.getPointers()); }
+    if (disabledInputs.length) {
+      this.drawDisabledInputs(disabledInputs);
+    }
+
+    if (inputs.length) {
+      this.drawInputs(inputs);
+    }
+
+    if (masks.length) {
+      this.drawMasks(masks);
+    }
+
+    if (showInput && this.showPointers) {
+      this.drawPointers(this.getPointers());
+    }
   };
 
-  DebugDrawPlugin.prototype.processObj = function processObj (obj, inputs, masks) {
-    this.drawObj(obj);
-
-    if (obj.input && this.showInput) {
-      inputs[inputs.length] = obj;
+  DebugDrawPlugin.prototype.processObj = function processObj (obj, disabledInputs, inputs, masks, showInput) {
+    if (obj.input && showInput) {
+      if (obj.input.enabled) {
+        inputs[inputs.length] = obj;
+      } else {
+        disabledInputs[disabledInputs.length] = obj;
+      }
     } else {
       this.drawObj(obj);
     }
@@ -98,20 +103,41 @@ var DebugDrawPlugin = /*@__PURE__*/(function (superclass) {
     this.systems = null;
   };
 
+  DebugDrawPlugin.prototype.drawDisabledInputs = function drawDisabledInputs (objs) {
+    this.graphic
+      .fillStyle(this.inputDisabledColor, this.alpha)
+      .lineStyle(this.lineWidth, this.inputDisabledColor, this.alpha);
+
+    objs.forEach(this.drawObjInput, this);
+  };
+
   DebugDrawPlugin.prototype.drawInputs = function drawInputs (objs) {
-    this.graphic.lineStyle(this.lineWidth, this.inputColor, this.alpha);
+    this.graphic
+      .fillStyle(this.inputColor, this.alpha)
+      .lineStyle(this.lineWidth, this.inputColor, this.alpha);
+
     objs.forEach(this.drawObjInput, this);
   };
 
   DebugDrawPlugin.prototype.drawMasks = function drawMasks (objs) {
-    this.graphic.lineStyle(this.lineWidth, this.maskColor, this.alpha);
+    this.graphic
+      .fillStyle(this.maskColor, this.alpha)
+      .lineStyle(this.lineWidth, this.maskColor, this.alpha);
+
     objs.forEach(this.drawObjMask, this);
   };
 
   DebugDrawPlugin.prototype.drawObj = function drawObj (obj) {
-    this.graphic
-      .strokeRect(getLeft(obj), getTop(obj), obj.displayWidth || obj.width, obj.displayHeight || obj.height)
-      .fillPoint(obj.x, obj.y, 3 * this.lineWidth);
+    var width = obj.displayWidth || obj.width;
+    var height = obj.displayHeight || obj.height;
+    var left = obj.originX ? (obj.x - obj.originX * width) : obj.x;
+    var top = obj.originY ? (obj.y - obj.originY * height) : obj.y;
+
+    this.graphic.fillPoint(obj.x, obj.y, 3 * this.lineWidth);
+
+    if (width || height) {
+      this.graphic.strokeRect(left, top, width, height);
+    }
 
     if (obj.rotation && this.showRotation) {
       this.drawObjRotation(obj);
@@ -119,11 +145,13 @@ var DebugDrawPlugin = /*@__PURE__*/(function (superclass) {
   };
 
   DebugDrawPlugin.prototype.drawObjRotation = function drawObjRotation (obj) {
+    var length = 0.5 * max((obj.displayWidth || obj.width), (obj.displayHeight || obj.height));
+
     this.graphic.lineBetween(
       obj.x,
       obj.y,
-      obj.x + 0.5 * cos(obj.rotation) * (obj.displayWidth || obj.width),
-      obj.y + 0.5 * sin(obj.rotation) * (obj.displayHeight || obj.height));
+      obj.x + cos(obj.rotation) * length,
+      obj.y + sin(obj.rotation) * length);
   };
 
   DebugDrawPlugin.prototype.drawObjInput = function drawObjInput (obj) {
@@ -141,8 +169,11 @@ var DebugDrawPlugin = /*@__PURE__*/(function (superclass) {
   DebugDrawPlugin.prototype.drawPointer = function drawPointer (pointer) {
     if (!pointer.active && !this.showInactivePointers) { return; }
 
-    var worldX = pointer.worldX;
-    var worldY = pointer.worldY;
+    var ref = this.systems.cameras.main;
+    var x = ref.x;
+    var y = ref.y;
+    var worldX = pointer.worldX - x;
+    var worldY = pointer.worldY - y;
 
     this.graphic.lineStyle(this.lineWidth, this.getColorForPointer(pointer), this.alpha);
 
@@ -152,10 +183,6 @@ var DebugDrawPlugin = /*@__PURE__*/(function (superclass) {
         .lineBetween(worldX, worldY, worldX + pointer.movementX, worldY + pointer.movementY);
     } else {
       this.graphic.strokeCircle(worldX, worldY, POINTER_RADIUS);
-    }
-
-    if (pointer.isDown) {
-      this.graphic.lineBetween(pointer.downX, pointer.downY, worldX, worldY);
     }
   };
 
@@ -168,19 +195,21 @@ var DebugDrawPlugin = /*@__PURE__*/(function (superclass) {
   };
 
   DebugDrawPlugin.prototype.getPointers = function getPointers () {
-    var ref = this.systems.input;
-    var mousePointer = ref.mousePointer;
-    var pointer1 = ref.pointer1;
-    var pointer2 = ref.pointer2;
-    var pointer3 = ref.pointer3;
-    var pointer4 = ref.pointer4;
-    var pointer5 = ref.pointer5;
-    var pointer6 = ref.pointer6;
-    var pointer7 = ref.pointer7;
-    var pointer8 = ref.pointer8;
-    var pointer9 = ref.pointer9;
+    var ref = this.systems;
+    var input = ref.input;
 
-    return [mousePointer, pointer1, pointer2, pointer3, pointer4, pointer5, pointer6, pointer7, pointer8, pointer9].filter(Boolean);
+    return [
+      input.mousePointer,
+      input.pointer1,
+      input.pointer2,
+      input.pointer3,
+      input.pointer4,
+      input.pointer5,
+      input.pointer6,
+      input.pointer7,
+      input.pointer8,
+      input.pointer9
+    ].filter(Boolean);
   };
 
   DebugDrawPlugin.prototype.bringToTop = function bringToTop () {
@@ -198,19 +227,16 @@ Object.assign(DebugDrawPlugin.prototype, {
   alpha: 1,
   color: 0x00ddff,
   inputColor: 0xffcc00,
+  inputDisabledColor: 0x886600,
   lineWidth: 1,
   maskColor: 0xff0022,
   pointerColor: 0xffcc00,
   pointerDownColor: 0x00ff22,
   pointerInactiveColor: 0x888888,
-  showPointers: true,
-  showInactivePointers: true,
+  showInactivePointers: false,
   showInput: true,
+  showPointers: true,
   showRotation: true
 });
-
-if (typeof window !== 'undefined') {
-  window.PhaserDebugDrawPlugin = DebugDrawPlugin;
-}
 
 module.exports = DebugDrawPlugin;
